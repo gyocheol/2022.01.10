f64c8dc8e62fb544a9f61821701e645d48e93be5
gyocheol <kys04171@naver.com>
2022-01-10 23:00:51 +0900

    python practice

---
+++ 1~20.py
@@ -0,0 +1,56 @@
+# 7,8번
+print('naver','kakao','sk','samsung', sep='/')
+
+# 9번
+print('first', end =''); print('second')
+
+# 10번
+print(5/3)
+print(f'{5/3:.2}')
+
+# 11.
+sj = 50000
+result = sj*10
+print(result)
+
+# 12.
+all = 298000000000000
+practice = 50000
+per = 15.79
+print(all, type(all))
+print(practice, type(practice))
+print(per, type(per))
+
+# 13.
+s = 'hello'
+t = 'python'
+print(s, t, end='',sep='! ')
+print('\n-----')
+print(s+'!',t)
+
+# 15.
+print(type('123'))
+
+# 16.
+# num_str = '720'
+# int = int(num_str)
+# print(int)
+
+# 17.
+num = 100
+num_str = str(num)
+print(num_str,type(num_str))
+
+# 18.
+a=15.79
+f = float(a)
+
+# 19.
+year = '2020'
+i = int(year)
+print(i-2, i-1, i)
+
+# 20.
+mon = 48584
+mul = 36
+print(mon*mul)
